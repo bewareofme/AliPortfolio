b9afc142a93801fcf339be525443761a90fab440
unknown <ali.nubeebukus@gmail.com>
2024-10-09 19:05:32 +0400

    to see deployment change live

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -39,7 +39,7 @@ const Contact = () => {
           <div className='xl:w-[54%] order-2 xl:order-none'>
             <form action="" className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
               <h3 className='text-4xl text-accent'>Let's work together</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolorum i!</p>
+              <p>I am excited to see this change</p>
               {/*input  */}
               <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 <Input type='firstname' placeholder='Firstname' />
